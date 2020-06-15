a6b9954d21d99151893a7f61368c894c0a47dcf6
levon <lstonoyan@gmail.com>
2020-06-15 12:56:31 +0400

    Initial commit

---
+++ src/main/java/com/example/openshiftdemo/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.openshiftdemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello im " + appName + " server";
+    }
+}
